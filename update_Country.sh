0f9ec39ecb92da12c0a46ff09f99f7d9ea6832cb
BiuBiu <95105272+BuBuxsy@users.noreply.github.com>
2023-06-08 02:26:44 +0800

    Update update_Country.sh

--- update_Country.sh
+++ update_Country.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p /tmp/loyalsoldier && curl --socks5 shanghai.mxsym.cn:7891 https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb > /tmp/loyalsoldier/Country.mmdb && \cp -rf /tmp/loyalsoldier/Country.mmdb /usr/share/GeoIP && rm -rf /tmp/loyalsoldier/Country.mmdb && echo 'update successful'
+mkdir -p /tmp/loyalsoldier && curl https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb > /tmp/loyalsoldier/Country.mmdb && \cp -rf /tmp/loyalsoldier/Country.mmdb /usr/share/GeoIP && rm -rf /tmp/loyalsoldier/Country.mmdb && echo 'update successful'
