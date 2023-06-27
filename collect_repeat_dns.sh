eb3db2ee210150d997555fb96ac3c249e50e161b
BiuBiu <95105272+BuBuxsy@users.noreply.github.com>
2023-06-28 03:15:37 +0800

    Add files via upload

--- collect_repeat_dns.sh
+++ collect_repeat_dns.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# 删除已存在的repeat_domain.txt文件
-#rm -r /etc/mosdns/rules/repeat_domain.txt
 
 # 读取包含域名的文本文件，每行一个域名
 domain_file="/etc/mosdns/mosdns.log"
@@ -10,13 +8,14 @@ domain_file="/etc/mosdns/mosdns.log"
 output_file="/etc/mosdns/rules/repeat_domain.txt"
 
 # 设置重复次数的阈值
-threshold=2000
+threshold=1000
 
-# 使用grep和正则表达式获取 "query" 之后的域名
+# 使用grep和正则表达式获取 "qname" 之后的域名
 # 排除数字、日期和标点符号，只查询域名网址
 # 使用sort和uniq -c对结果进行统计和排序
 # 结果保存到临时文件temp.txt
-grep -oE '"query": "([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})' "$domain_file" | sed 's/"query": "//' | sort | uniq -c | sort -rn > temp.txt
+grep -oE '"qname": "([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})' "$domain_file" | sed 's/"qname": "//' | sort | uniq -c | sort -rn > temp.txt
+
 
 # 从临时文件中读取结果，并生成域名列表
 # 同时将重复次数超过阈值的域名保存到输出文件
@@ -34,11 +33,10 @@ done < temp.txt
 rm temp.txt
 
 # 检查重复域名的数量是否小于阈值，给出相应的提示
-total_domains=$(grep -c '^' "$output_file")
-if (( total_domains < threshold )); then
+if ((duplicate_domains < threshold)); then
   echo "发现的重复域名数量少于阈值。"
+else
+  echo "查询完成，请查看 $output_file 文件。"
+  # 清空日志文件
+  cat /dev/null > "$domain_file"
 fi
-
-echo "查询完成，请查看 $output_file 文件。"
-
-
