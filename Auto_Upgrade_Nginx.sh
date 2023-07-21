54b84b656c3b353cbf58036dc4dec352d1020fd4
BiuBiu <95105272+BuBuxsy@users.noreply.github.com>
2023-07-22 03:02:51 +0800

    Update Auto_Upgrade_Nginx.sh

--- Auto_Upgrade_Nginx.sh
+++ Auto_Upgrade_Nginx.sh
@@ -49,13 +49,13 @@ ngx_http_geoip2_module_source() {
 }
 
 pcre2_source() { 
-    git clone https://github.com/PCRE2Project/pcre2.git --recurse-submodules --depth=1
+    git clone https://github.com/PCRE2Project/pcre2.git --recurse-submodules #--depth=1
     #wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.42/pcre2-10.42.tar.gz --no-check-certificate
     #tar -zxf pcre2-10.42.tar.gz && mv pcre2-10.42 pcre2 && rm -f pcre2-10.42.tar.gz
 }
 
 quictls_source() {
-    git clone https://github.com/quictls/openssl -b openssl-3.0.8+quic quictls --recurse-submodules --depth=1 
+    git clone https://github.com/quictls/openssl -b openssl-3.0.9+quic quictls --recurse-submodules --depth=1 
 }
 
 #libressl_source(){
