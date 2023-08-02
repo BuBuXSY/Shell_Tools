ff4b1f1ab7a46291a40b04f09ecab07f98f5c0e7
BiuBiu <95105272+BuBuxsy@users.noreply.github.com>
2023-08-03 04:23:02 +0800

    Update Auto_Upgrade_Nginx.sh

--- Auto_Upgrade_Nginx.sh
+++ Auto_Upgrade_Nginx.sh
@@ -55,7 +55,7 @@ ngx_http_geoip2_module_source() {
 #}
 
 quictls_source() {
-    git clone https://github.com/quictls/openssl -b openssl-3.0.9+quic quictls --recurse-submodules --depth=1 
+    git clone https://github.com/quictls/openssl -b openssl-3.1.2+quic quictls --recurse-submodules --depth=1 
 }
 
 #libressl_source(){
@@ -211,7 +211,7 @@ ngx_http_geoip2_module_source() {
 #}
 
 quictls_source() {
-    git clone https://github.com/quictls/openssl -b openssl-3.0.9+quic quictls --recurse-submodules --depth=1 
+    git clone https://github.com/quictls/openssl -b openssl-3.1.2+quic quictls --recurse-submodules --depth=1 
 }
 
 #libressl_source(){
