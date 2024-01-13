bc2ceeff3f33a3c7091615db9b0415c1515aff29
BuBuXSY <95105272+BuBuXSY@users.noreply.github.com>
2024-01-14 00:21:05 +0800

    Update update_Country.sh

--- update_Country.sh
+++ update_Country.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p /tmp/loyalsoldier && curl https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb > /tmp/loyalsoldier/Country.mmdb && \cp -rf /tmp/loyalsoldier/Country.mmdb /usr/share/GeoIP && rm -rf /tmp/loyalsoldier/Country.mmdb && echo 'update successful'
+mkdir -p /tmp/loyalsoldier && curl https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb > /tmp/loyalsoldier/Country.mmdb && \cp -rf /tmp/loyalsoldier/Country.mmdb /usr/share/geoip && rm -rf /tmp/loyalsoldier/Country.mmdb && echo 'update successful'
