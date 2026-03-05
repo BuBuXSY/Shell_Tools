7c71f05b45e5f8e81e5e84fc432d129fa0878f67
BuBuXSY <95105272+BuBuXSY@users.noreply.github.com>
2026-03-06 00:00:34 +0800

    Update install_cert.sh

--- install_cert.sh
+++ install_cert.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
+# ====================================================
+# MIT License
+#
+# Copyright (c) 2025 BuBuXSY
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ====================================================
 # SSL证书管理工具，支持多CA，DNS API/手动验证，ECC证书，自动部署并重载nginx
 # 支持多CA，DNS API/手动验证，ECC证书，自动部署并重载nginx
 # By: BuBuXSY
 # Version: 2025-09-25
-# License: MIT
 
 set -euo pipefail  # 严格模式
 
