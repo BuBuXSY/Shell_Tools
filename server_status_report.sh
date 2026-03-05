fb453aa1db84d9193b7d7780442d2d4008125bbe
BuBuXSY <95105272+BuBuXSY@users.noreply.github.com>
2026-03-05 23:59:44 +0800

    Update server_status_report.sh

--- server_status_report.sh
+++ server_status_report.sh
@@ -1,8 +1,31 @@
-#通过企业微信机器人来推送服务器状态的脚本
+#!/bin/bash
+# ====================================================
+# MIT License
+#
+# Copyright (c) 2025 BuBuXSY
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ====================================================
 # By: BuBuXSY
 # Version: 2025-07-03
 
-#!/bin/bash
+
 ######################################################################################
 WEBHOOK_URL="https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=你的企业微信机器人KEY"
 ######################################################################################
